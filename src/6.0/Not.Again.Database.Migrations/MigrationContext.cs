7aa3cbbb606cb026cbe12a9c90d7fe6fd385bfd8
Gabriel Palma <gubpalma@hotmail.com>
2024-08-20 13:11:49 +1000

    Added db migration

---
+++ src/6.0/Not.Again.Database.Migrations/MigrationContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Not.Again.Database.Migrations
+{
+    public class MigrationContext : NotAgainDbContext
+    {
+        public MigrationContext() {}
+
+        public MigrationContext(DbContextOptions<NotAgainDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration =
+                    new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+                optionsBuilder
+                    .UseSqlServer(
+                        configuration.GetConnectionString("NOT-AGAIN"),
+                        options => options.MigrationsAssembly("Not.Again.Database.Migrations")
+                    );
+            }
+        }
+    }
+}
---
+++ src/6.0/Not.Again.Database.Migrations/Migrations/20240820031016_Initial.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Not.Again.Database.Migrations.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestAssembly",
+                columns: table => new
+                {
+                    TestAssemblyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestAssemblyName = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestAssembly", x => x.TestAssemblyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestRecord",
+                columns: table => new
+                {
+                    TestRecordId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestAssemblyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClassName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    MethodName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    TestName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    DelimitedTestArguments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastHash = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestRecord", x => x.TestRecordId);
+                    table.ForeignKey(
+                        name: "FK_TestRecord_TestAssembly_TestAssemblyId",
+                        column: x => x.TestAssemblyId,
+                        principalTable: "TestAssembly",
+                        principalColumn: "TestAssemblyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestRun",
+                columns: table => new
+                {
+                    TestRunId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestRecordId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RunDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Result = table.Column<int>(type: "int", nullable: false),
+                    TotalDuration = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestRun", x => x.TestRunId);
+                    table.ForeignKey(
+                        name: "FK_TestRun_TestRecord_TestRecordId",
+                        column: x => x.TestRecordId,
+                        principalTable: "TestRecord",
+                        principalColumn: "TestRecordId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestRecord_TestAssemblyId",
+                table: "TestRecord",
+                column: "TestAssemblyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestRun_TestRecordId",
+                table: "TestRun",
+                column: "TestRecordId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestRun");
+
+            migrationBuilder.DropTable(
+                name: "TestRecord");
+
+            migrationBuilder.DropTable(
+                name: "TestAssembly");
+        }
+    }
+}
