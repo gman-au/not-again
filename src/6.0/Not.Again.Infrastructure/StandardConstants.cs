2baeb6cdaab77e3eccba9a408765f0a140d87ce2
Gabriel Palma <gubpalma@hotmail.com>
2025-08-22 11:33:53 +1000

    feat: added postgres support

--- src/6.0/Not.Again.Infrastructure/StandardConstants.cs
+++ src/6.0/Not.Again.Infrastructure/StandardConstants.cs
@@ -2,8 +2,9 @@
 {
     public static class StandardConstants
     {
-        public const string ConnectionStringVariableName = "ConnectionStrings__NOT-AGAIN";
-        
+        public const string ConnectionStringVariableNameSqlServer = "ConnectionStrings__NOT-AGAIN-SQL-SERVER";
+        public const string ConnectionStringVariableNamePostgreSql = "ConnectionStrings__NOT-AGAIN-SQL-POSTGRESQL";
+
         public const string BaseUrlVariableName = "NOT_AGAIN_URL";
         public const string RerunTestsOlderThanDaysVariableName = "RERUN_TESTS_OLDER_THAN_DAYS";
         public const int DefaultRerunTestsOlderThanDays = 0;
